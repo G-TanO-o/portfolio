b9750377d62671e8dc18d8098a0ce1d3d2ccd8f1
gwen-bo <gwen.bogaert@hotmail.com>
2020-07-09 16:51:57 +0200

    changed page title and added Filmpje component (resulted in last error)

---
+++ src/components/Filmpje/index.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { useObserver } from "mobx-react-lite";
+import styles from "../Filmpje/filmpje.module.css";
+import { useParams } from "react-router-dom";
+import {useStores} from "../../hooks";
+
+
+
+const Filmpje = () => {
+    const { id } = useParams();    
+    const {dataStore} = useStores();
+    const work = dataStore.selectWorkById(id);
+    return useObserver( () => 
+        <>
+        <video src={`../assets/img/projects/${work.filmpje}`} width="960" height="540" controls alt="Video thats explains the app."></video>
+        </>
+    )
+    
+}
+
+export default Filmpje;
